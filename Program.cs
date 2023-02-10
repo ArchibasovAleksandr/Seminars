6125ceb0ce66810910ed0046ff6c6a33576944a6
Archibasov2023 <arch1405@yandex.ru>
2023-02-10 22:57:11 +0400

    add КОД!!!

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"(quad of {num} is {quad}");
+// Задача 2.
+
+// Console.WriteLine("Input ferst number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+// if (quad == num1)
+// {
+//     Console.WriteLine("Ferst number is quad of second number");
+// }
+// else
+// {
+//     Console.WriteLine("Ferst num is not quad of sec num");
+// }
+// Задача 3.
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+
+// while (current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+// Задача 4.
+
+// Console.WriteLine("Input tree-digit number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num % 10;
+
+// Console.WriteLine($"Last number of {num} is {result}");
+// Задача5.
+
+
